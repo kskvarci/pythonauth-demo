89d955bf3fae673f5a9816931d3d95c63885034a
kskvarci <kskvarci@gmail.com>
2019-11-24 11:47:35 -0500

    eliminated a few params

--- auth.py
+++ auth.py
@@ -21,24 +21,29 @@ else:
 authority_url = (sample_parameters['AuthorityHostUrl'] + '/' + sample_parameters['Tenant'])
 
 # uncomment for verbose logging
-turn_on_logging()
+# turn_on_logging()
 
 context = adal.AuthenticationContext(
     authority_url, api_version=1.0,
     )
 
+# Pull user, client and api server IDs from existing config
+ClusterUser = subprocess.check_output(['kubectl', 'config', 'view', '--output', 'jsonpath={.users[0].name}'])
+ClientId = subprocess.check_output(['kubectl', 'config', 'view', '--output', 'jsonpath={.users[0].user.auth-provider.config.client-id}'])
+ServerId = subprocess.check_output(['kubectl', 'config', 'view', '--output', 'jsonpath={.users[0].user.auth-provider.config.apiserver-id}'])
+
 # fetch token using adal
 token = context.acquire_token_with_username_password(
-    sample_parameters['Resource'],
+    ServerId.decode('ASCII'),
     sample_parameters['UserName'],
     sample_parameters['Password'],
-    sample_parameters['ClientId'])
+    ClientId.decode('ASCII'))
 
 # use kubetl to append token values to config
 subprocess.run(['kubectl', \
     'config', \
     'set-credentials', \
-    'clusterUser_' + sample_parameters["RGName"] + '_' + sample_parameters["ClusterName"], \
+    ClusterUser.decode('ASCII'), \
     '--auth-provider-arg=expires-on=' + str(int(datetime.timestamp(datetime.strptime(token["expiresOn"],"%Y-%m-%d %H:%M:%S.%f")))), \
     '--auth-provider-arg=expires-in=' + str(token["expiresIn"]), \
     '--auth-provider-arg=access-token=' + token["accessToken"], \
